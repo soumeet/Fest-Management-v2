9fa592fda46ba3213f5c800652bd57b5989a5865
soumeet <soumeet.basak@gmail.com>
2016-04-04 18:54:20 +0530

    commit_04042016_1853
    do

--- php/event_search.php
+++ php/event_search.php
@@ -12,33 +12,34 @@
 			$sql = "SELECT * FROM db_groups WHERE event = '$event'";
 		}
 		$result = mysqli_query($con, $sql);
-        $row = mysqli_fetch_all($result);
-        /*while ($row = mysqli_fetch_array($result)){
+        //$row = mysqli_fetch_all($result);
+        echo "[";
+        $i=1;
+        while ($row = mysqli_fetch_array($result)){
             if($row['group_id'] < 100)
                $id='SNKLP00'.$row['group_id'];
             else if($row['group_id'] < 1000)
                $id='SNKLP0'.$row['group_id'];
             $name=$row['name'];
-            $mem1=$row['member1'];
-            $mem2=$row['member2'];
-            $mem3=$row['member3'];
-            $mem4=$row['member4'];
-            $mem5=$row['member5'];
+            $event=$row['event'];
+            $mem1 = mysqli_fetch_row(mysqli_query($con, "SELECT name FROM db_users WHERE id = '$row[member1]'"))[0];
+            $mem2 = mysqli_fetch_row(mysqli_query($con, "SELECT name FROM db_users WHERE id = '$row[member2]'"))[0];
+            $mem3 = mysqli_fetch_row(mysqli_query($con, "SELECT name FROM db_users WHERE id = '$row[member3]'"))[0];
+            $mem4 = mysqli_fetch_row(mysqli_query($con, "SELECT name FROM db_users WHERE id = '$row[member4]'"))[0];
+            $mem5 = mysqli_fetch_row(mysqli_query($con, "SELECT name FROM db_users WHERE id = '$row[member5]'"))[0];
             
             //echo  $id." ".$name." ".$mem1." ".$mem2." ".$mem3." ".$mem4." ".$mem5."\r\n";
+            echo  "[\"".$id."\",\"".$name."\",\"".$event."\",\"".$mem1."\",\"".$mem2."\",\"".$mem3."\",\"".$mem4."\",\"".$mem5."\"]";
             
-            $data=array('res' => '1',
-                           'id' => $id,
-                           'name' => $name,
-                           'mem1' => $mem1,
-                           'mem2' => $mem2,
-                           'mem3' => $mem3,
-                           'mem4' => $mem4,
-                           'mem5' => $mem5);
+            if(($i+1)==mysqli_num_rows($result)){
+                echo ",";
+                $i=$i+1;
+            }
             
         }
-        $js_str = json_encode($data);*/
-        echo json_encode($row);
+        echo "]";
+        //echo json_encode($data);
+        //echo json_encode($row);
         exit;
     }   
 ?>
