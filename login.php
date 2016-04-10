981d4c35c808b8ef09a897dca7ab5a3f281f6167
soumeet <soumeet.basak@gmail.com>
2016-04-11 01:12:15 +0530

    done!

--- login.php
+++ login.php
@@ -10,6 +10,7 @@ if(!empty($_SESSION['sid']))
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/lato.css">
   <link rel="stylesheet"  type="text/css" href="css/montserrat.css" >
   <link rel="stylesheet" href="css/dynamics.css">
 
